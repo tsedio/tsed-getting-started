bc8327afdd3441a4c315844cccba1a35228e3110
Travis CI User <travis@example.org>
2019-09-19 09:12:56 +0000

    Deploy project with Ts.ED v5.31.2

--- src/Server.ts
+++ src/Server.ts
@@ -1,5 +1,6 @@
 import {GlobalAcceptMimesMiddleware, ServerLoader, ServerSettings} from "@tsed/common";
 import "@tsed/swagger";
+import {join} from "path";
 import {RestCtrl} from "./controllers/RestCtrl";
 
 const cookieParser = require("cookie-parser");
@@ -11,6 +12,7 @@ const rootDir = __dirname;
 @ServerSettings({
   rootDir,
   acceptMimes: ["application/json"],
+  httpPort: 8083,
   logger: {
     debug: true,
     logRequest: true,
@@ -22,15 +24,23 @@ const rootDir = __dirname;
       `${rootDir}/controllers/**/*.ts` // Automatic Import, /!\ doesn't works with webpack/jest, use  require.context() or manual import instead
     ]
   },
-  swagger: {
-    path: "/api-docs"
-  },
+  swagger: [
+    {
+      path: "/api-docs"
+    }
+  ],
   calendar: {
     token: true
   },
-  debug: true
+  statics: {
+    "/statics": join(__dirname, "..", "statics")
+  }
 })
 export class Server extends ServerLoader {
+  constructor(settings) {
+    super(settings);
+  }
+
   /**
    * This method let you configure the middleware required by your application to works.
    * @returns {Server}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import {ServerLoader, $log} from "@tsed/common";
+import {$log, ServerLoader} from "@tsed/common";
 import {Server} from "./Server";
 
 async function bootstrap() {
   try {
     $log.debug("Start server...");
-    const server = await ServerLoader.bootstrap(Server);
+    const server = await ServerLoader.bootstrap(Server, {});
 
     await server.listen();
     $log.debug("Server initialized");
